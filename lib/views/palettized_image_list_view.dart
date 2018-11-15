b56d7f429fc2e861427f0fba652b8a571cac609d
Matt Cialini <matt.cialini@gmail.com>
2018-11-14 22:16:39 -0500

    New screen!

--- lib/views/palettized_image_list_view.dart
+++ lib/views/palettized_image_list_view.dart
@@ -3,33 +3,20 @@ Responsible for showing:
 - list of PalettizeImage instances
 - FloatingActionButtons to begin creation process
 */
+import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 import '../models/palettized_image.dart';
+import 'palettized_image_view.dart';
 
 
-class PalettizedImageListView extends StatefulWidget {
-  @override
-  _PalettizedImageListViewState createState() => _PalettizedImageListViewState();
-}
-
-class _PalettizedImageListViewState extends State<PalettizedImageListView> {
-  PalettizedImage _image;
-
-  Future getImageCamera() async {
-    var rawImage = await ImagePicker.pickImage(source: ImageSource.camera);
-
-    setState(() {
-      _image = new PalettizedImage(rawImage);
-    });
+class PalettizedImageListView extends StatelessWidget {
+  Future<File> getImageCamera() async {
+    return await ImagePicker.pickImage(source: ImageSource.camera);
   }
 
-  Future getImageGallery() async {
-    var rawImage = await ImagePicker.pickImage(source: ImageSource.gallery);
-
-    setState(() {
-      _image = new PalettizedImage(rawImage);
-    });
+  Future<File> getImageGallery() async {
+    return await ImagePicker.pickImage(source: ImageSource.gallery);
   }
 
   @override
@@ -38,22 +25,40 @@ class _PalettizedImageListViewState extends State<PalettizedImageListView> {
       appBar: AppBar(
         title: Text('Palettize'),
       ),
-      body: Center(
-        child: _image == null
-            ? Text('No image selected.')
-            : Image.file(_image.rawImage),
-      ),
+      // body: Center(
+      //   child: _image == null
+      //       ? Text('No image selected.')
+      //       : Image.file(_image.rawImage),
+      // ),
       floatingActionButton: Column(
         crossAxisAlignment: CrossAxisAlignment.end,
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
           FloatingActionButton(
+            heroTag: 'addPhoto',
             child: Icon(Icons.add_a_photo),
-            onPressed: getImageCamera
+            onPressed: () async {
+              File rawImage = await getImageCamera();
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PalettizedImageView(rawImage)
+                )
+              );
+            }
           ),
           FloatingActionButton(
+            heroTag: 'choosePhoto',
             child: Icon(Icons.photo_library),
-            onPressed: getImageGallery
+            onPressed: () async {
+              File rawImage = await getImageGallery();
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PalettizedImageView(rawImage)
+                )
+              );
+            }
           )
         ]
       ),
--- lib/views/palettized_image_view.dart
+++ lib/views/palettized_image_view.dart
@@ -8,3 +8,45 @@ Responsible for showing:
 - a close icon
 - a save icon
 */
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/material.dart';
+import '../models/palettized_image.dart';
+
+
+class PalettizedImageView extends StatelessWidget {
+  final File rawImage;
+
+  PalettizedImageView(this.rawImage);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: new IconButton(
+          icon: Icon(Icons.close),
+          tooltip: 'Cancel',
+          onPressed: () {
+            print('CANCELLED');
+            Navigator.pop(context);
+          },
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            tooltip: 'Save',
+            onPressed: () {
+              print('SAVED');
+              Navigator.pop(context);
+            },
+          )
+        ]
+      ),
+      body: Center(
+        child: this.rawImage == null
+            ? Text('No image selected.')
+            : Image.file(rawImage),
+      ),
+    );
+  }
+}
